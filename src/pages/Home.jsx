bf2b158be7391ff2bcfd69f1d2d22aecb0383948
ZainHassan <Zainulhassan14405@gmail.com>
2026-04-02 01:20:05 +0500

    Modifying home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Carousel from "../components/Carousel";
 import Controls from "../components/Controls";
 import Dots from "../components/Dots";
 
-function Home() {
+function Homelayout() {
   const images = [
     "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?auto=format&fit=crop&w=500&q=80",
     "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=500&q=80",
@@ -30,4 +30,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Homelayout;
